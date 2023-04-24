ef8cee092166c9abd720c5a81b326ad18666cb03
divinityMaker <truedmaker@gmail.com>
2023-04-24 20:26:52 -0300

    feat: basic modal

---
+++ src/app/components/Modal.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useState } from "react";
+
+interface IModal {
+    title?: string;
+    bodyContent?: React.ReactElement;
+    isOpen?: boolean;
+    footer?: React.ReactElement;
+    onClose?: () => void;
+    onSubmit?: () => void;
+}
+
+const Modal: React.FC<IModal> = ({
+    title,
+    bodyContent,
+    isOpen,
+    footer,
+    onClose,
+    onSubmit
+}) => {
+
+    const [showModal, setShowModal] = useState(isOpen);
+
+    return (
+        <div className="overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70 flex justify-center items-center">
+            <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full lg:h-auto md:h-auto">
+                <div
+                className={`
+                translante 
+                duration-300 
+                h-full 
+                ${showModal ? "translate-y-0" : "translate-y-full"}
+                ${showModal ? "opacity-100" : "opacity-0"}
+            `}
+                >
+                    <div className="translate h-full lg:h-auto md:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                        s
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Modal;
